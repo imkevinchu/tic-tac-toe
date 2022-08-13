0c4a3ab210818e35de1c3e97249aed1365506453
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-08-12 19:42:44 -0700

    layout game board

---
+++ script.js
@@ -0,0 +1,14 @@
+// const playerCreator = () => {
+//   return {};
+// };
+
+// const gameBoard = (() => {
+//   const checkGameOver = () => {};
+//   return {};
+// })();
+
+// const displayController = (() => {
+//   return {};
+// })();
+
+window.onload = () => {};
