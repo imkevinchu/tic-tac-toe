b8073943f114002c550dcde11c3bd77a947a69ca
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-08-12 21:45:25 -0700

    marking tiles on board

--- script.js
+++ script.js
@@ -1,25 +1,160 @@
 const playerCreator = (name, sign) => {
-  this.name = name;
-  this.sign = sign;
-
-  const getName = () => {
-    return name;
-  };
-
-  const getSign = () => {
-    return sign;
-  };
-
-  return { getName, getSign };
+  return { name, sign };
 };
 
+const gameController = (() => {
+  let gameOver = false;
+
+  const isGameOver = () => {
+    gameOver;
+  };
+
+  const setGameOver = () => {
+    gameOver = true;
+  };
+
+  return {
+    isGameOver,
+    setGameOver,
+  };
+})();
+
 const gameBoard = (() => {
-  const checkGameOver = () => {};
-  return {};
-})();
+  let board = [];
 
-const displayController = (() => {
-  return {};
-})();
+  board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
 
-window.onload = () => {};
+  const playerO = playerCreator("Player 1", "O");
+  const playerX = playerCreator("Player 2", "X");
+  let currPlayer = playerO;
+
+  const checkWinner = () => {
+    //horizontally, check 3 rows
+    for (let r = 0; r < 3; r++) {
+      if (
+        board[r][0] == board[r][1] &&
+        board[r][1] == board[r][2] &&
+        board[r][0] != " "
+      ) {
+        //if we found the winning row
+        //apply the winner style to that row
+        for (let i = 0; i < 3; i++) {
+          let tile = document.getElementById(r.toString() + "-" + i.toString());
+          tile.classList.add("winner");
+        }
+        gameController.setGameOver;
+        return;
+      }
+    }
+
+    //vertically, check 3 columns
+    for (let c = 0; c < 3; c++) {
+      if (
+        board[0][c] == board[1][c] &&
+        board[1][c] == board[2][c] &&
+        board[0][c] != " "
+      ) {
+        //if we found the winning col
+        //apply the winner style to that col
+        for (let i = 0; i < 3; i++) {
+          let tile = document.getElementById(i.toString() + "-" + c.toString());
+          tile.classList.add("winner");
+        }
+        gameController.setGameOver;
+        return;
+      }
+    }
+
+    //diagonally
+    if (
+      board[0][0] == board[1][1] &&
+      board[1][1] == board[2][2] &&
+      board[0][0] != " "
+    ) {
+      for (let i = 0; i < 3; i++) {
+        let tile = document.getElementById(i.toString() + "-" + i.toString());
+        tile.classList.add("winner");
+      }
+      gameController.setGameOver;
+      return;
+    }
+
+    //anti-diagonally
+    if (
+      board[0][2] == board[1][1] &&
+      board[1][1] == board[2][0] &&
+      board[0][2] != " "
+    ) {
+      //0-2
+      let tile = document.getElementById("0-2");
+      tile.classList.add("winner");
+
+      //1-1
+      tile = document.getElementById("1-1");
+      tile.classList.add("winner");
+
+      //2-0
+      tile = document.getElementById("2-0");
+      tile.classList.add("winner");
+      gameController.setGameOver;
+      return;
+    }
+  };
+
+  function setTile() {
+    console.log("click");
+
+    // if (gameController.isGameOver) {
+    //   return;
+    // }
+
+    console.log(currPlayer);
+
+    let coords = this.id.split("-"); //ex) "1-2" -> ["1", "2'"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (board[r][c] != " ") {
+      //already taken spot
+      return;
+    }
+
+    board[r][c] = currPlayer.sign; //mark the board
+    this.innerText = currPlayer.sign; //mark the board on html
+
+    const switchCurrPlayer = () => {
+      //change players
+      if (currPlayer == playerO) {
+        currPlayer = playerX;
+      } else {
+        currPlayer = playerO;
+      }
+    };
+
+    switchCurrPlayer();
+    checkWinner();
+  }
+
+  for (let r = 0; r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      if (r == 0 || r == 1) {
+        tile.classList.add("horizontal");
+      }
+      if (c == 0 || c == 1) {
+        tile.classList.add("vertical");
+      }
+      tile.innerText = "";
+      tile.addEventListener("click", setTile);
+      document.getElementById("gameboard").appendChild(tile);
+    }
+  }
+
+  return { board };
+})();
